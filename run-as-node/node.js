a9017407be2847be2176763ec91a33c921c82d85
Benton J <phixmeplease@gmail.com>
2022-06-24 06:50:00 -0400

    add run-as-node basics

--- run-as-node/node.js
+++ run-as-node/node.js
@@ -2,4 +2,12 @@
     Run-As-Node Project
     node.js
 */
+const express = require('express')
+const log = require('./utils/log')
 
+log(`
+Run-As-Node
+=============
+
+This project acts as the node for hosting containers using the astro-containers project!
+`, 'yellow')
